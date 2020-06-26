3368efd3fca89cc8cb7d60ba2ff1afc08c97f022
Keir Pearson <58754415+KeirSP@users.noreply.github.com>
2020-06-26 16:30:40 +0100

    Update binary-search.js

--- binary-search.js
+++ binary-search.js
@@ -1,1 +1,10 @@
+//binary search is a way of searching through ORDERED data by determining where the search object is located in the data and using a process of "halving" each time to get there
+//e.g for finding number 8 in an array of numbers 1 - 10, start at five, then 7, then 8.
+//this is much faster than linear search (going up one by one from the first entry)
+
+//let min = 1 and max = n
+//guess the avg of min and max, rounded down
+//too low? set min as guessed number +1
+//too high? set max as guessed number -1
+
 
