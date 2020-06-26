77f31ddf85b1fcd5ef6ab7378f3ed2e4117d9367
Keir Pearson <58754415+KeirSP@users.noreply.github.com>
2020-06-26 17:13:43 +0100

    Update binary-search.js

--- binary-search.js
+++ binary-search.js
@@ -7,4 +7,32 @@
 //too low? set min as guessed number +1
 //too high? set max as guessed number -1
 
+var doSearch = function(array, targetValue) {
+	var min = 0;
+	var max = array.length - 1;
+    var guess;
+    var numOfGuesses = 1;
+    
+    while(max >= min) {
+        guess = Math.floor((max + min)/2);
+        if(array[guess] === targetValue){
+            println("You guessed in "+ numOfGuesses + " attempts");
+            return guess;
+        }
+        else if(array[guess] < targetValue) {
+            min = guess +1;
+        }
+        else {
+            max = guess - 1;
+        }
+        numOfGuesses += 1;
+        println(guess);
+    }
+	return -1;
+};
 
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 73);
+println("Found prime at index " + result);
